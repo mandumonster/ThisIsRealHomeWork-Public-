c38e87e2a3dbc57f99ba18902ff17714eb472beb
mzgz <minjugim639@gmail.com>
2023-11-22 14:46:58 +0900

    1122지니뮤직 제출

---
+++ 1122homework.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+for i in range(4):
+  num=i+1
+  header={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+  url=f'https://www.genie.co.kr/chart/top200?ditc=D&ymd=20231122&hh=14&rtm=Y&pg={num}'
+  request=requests.get(url,headers=header)
+  soup = BeautifulSoup(request.text)
+  table=soup.find('table',{'class':'list-wrap'})
+  artists=table.findAll('a',{'class':'artist'})
+  titles=table.findAll('a',{'class':'title'})
+  for i, (t, a) in enumerate(zip(titles, artists)):
+    title = t.text.replace('19금\n', '').strip()
+    artist = a.text.strip().split('\n')[0]
+    print('{0:3d}위 {1} - {2}'.format((num-1)*50+i+1, artist, title))
